2136963ffe54d8f023cae0c0571e2c4b48883c6f
Heldragon <liannerequesto@gmail.com>
2023-07-03 05:49:06 +0800

    added login and signu up pages

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 from flask import Flask, render_template, request
+from wtforms import Form, BooleanField, StringField, PasswordField,validators
 from pyzbar.pyzbar import decode
 from PIL import Image
 
 app = Flask(__name__)
 
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    firstname = StringField('First Name', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    lastname = StringField('Last Name', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.DataRequired(), validators.Length(min=6, max=55)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+
 @app.route("/")
 def index():
   return render_template('home.html')
 
-@app.route("/loginpage")
+@app.route("/loginpage", methods=['GET', 'POST'])
 def login():
   return render_template('loginpage.html')
 
-@app.route("/signuppage")
+@app.route("/signuppage", methods=['GET', 'POST'])
 def signup():
-  return render_template('signup_form.html')
+  form = RegistrationForm()
+
+  if form.validate():
+     return '<h1>' + form.username.data + '</h1>'
+  
+  return render_template('signup_form.html', form=form)
 
 @app.route("/pricechecker")
 def pricechecker():
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 
-db_connection_string ="" #need to change password
+db_connection_string = ""
 
 engine = create_engine(
   db_connection_string,
@@ -13,6 +13,13 @@ engine = create_engine(
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from inventory"))
+  print("type(result):", type(result))
+  result_all = result.all()
+  print("type(result.all()):", type(result_all))
+  print("result.all():", result_all)
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from userinfo"))
   print("type(result):", type(result))
   result_all = result.all()
   print("type(result.all()):", type(result_all))
